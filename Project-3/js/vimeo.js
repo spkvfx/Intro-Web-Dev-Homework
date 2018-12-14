fa573c6a1a19b0d64331c7d076c3544cd2c6f0b0
shawn kearney <spk.vfx@gmail.com>
2018-12-14 16:35:42 -0700

    sk

---
+++ Project-3/js/vimeo.js
@@ -0,0 +1,18 @@
+//taken from https://stackoverflow.com/questions/1361149/get-img-thumbnails-from-vimeo
+
+function vimeoLoadingThumb(id){
+    var url = "http://vimeo.com/api/v2/video/" + id + ".json?callback=showThumb";
+
+    var id_img = "#vimeo-" + id;
+
+    var script = document.createElement( 'script' );
+    script.src = url;
+
+    $(id_img).before(script);
+}
+
+
+function showThumb(data){
+    var id_img = "#vimeo-" + data[0].id;
+    $(id_img).attr('src',data[0].thumbnail_medium);
+}
